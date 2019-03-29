fb6e3e47bc17e40375036811bf3ec990f67e6df3
Leonardo Oliveira <leozartino@gmail.com>
2019-03-29 17:16:05 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+let meuVetor = []
+
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    array.push(`I am ${i} strange loop${i === 0 || i === 1 ? '' : 's'}.`)
+  }
+  return array;
+}
+
+function whileLoop(n){
+  
+}
